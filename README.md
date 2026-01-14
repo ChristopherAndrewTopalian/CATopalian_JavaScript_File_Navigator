432e29dcfe712c130f569a760883b8ce9bbffa99
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-01-14 18:45:57 -0500

    update

--- README.md
+++ README.md
@@ -40,7 +40,7 @@ Use App: https://christopherandrewtopalian.github.io/CATopalian_JavaScript_File_
 //----//
 
 // Dedicated to God the Father  
-// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2025  
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2026  
 // https://github.com/ChristopherTopalian  
 // https://github.com/ChristopherAndrewTopalian  
 // https://sites.google.com/view/CollegeOfScripting  
--- src/js/4make/makeTitleOfApp.js
+++ src/js/4make/makeTitleOfApp.js
@@ -3,9 +3,6 @@
 function makeTitleOfApp()
 {
     let theLink = ce('a');
-    theLink.style.position = 'absolute';
-    theLink.style.right = 7 + 'px';
-    theLink.style.top = 2 + 'px';
     theLink.href = 'https://github.com/ChristopherAndrewTopalian/CATopalian_JavaScript_File_Navigator';
     theLink.target = '_blank';
     theLink.textContent = 'CATopalian JavaScript File Navigator';
@@ -15,13 +12,13 @@ function makeTitleOfApp()
     theLink.style.fontWeight = 'bold';
     theLink.style.textDecoration = 'none';
     theLink.style.zIndex = 1;
-    ba( theLink);
+    return theLink;
 }
 
 //----//
 
 // Dedicated to God the Father  
-// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2025  
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2026  
 // https://github.com/ChristopherTopalian  
 // https://github.com/ChristopherAndrewTopalian  
 // https://sites.google.com/view/CollegeOfScripting
